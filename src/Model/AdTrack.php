194baeb698a0831da3d713d75ec4d92eb84de42a
Shaughn Le Grange - Hatlen <shaughn@silvertreebrands.com>
2024-03-11 13:13:37 +0200

    PHPCS cleanup and bug fix on db schema primary

--- src/Model/AdTrack.php
+++ src/Model/AdTrack.php
@@ -35,37 +35,6 @@ class AdTrack extends AbstractExtensibleModel implements AdTrackInterface, Ident
     protected $_eventPrefix = self::CACHE_TAG;
 
     /**
-     * Construct
-     *
-     * @param \Magento\Framework\Model\Context $context
-     * @param \Magento\Framework\Registry $registry
-     * @param \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory
-     * @param \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory
-     * @param \Magento\Framework\Model\ResourceModel\AbstractResource|null $resource
-     * @param \Magento\Framework\Data\Collection\AbstractDb|null $resourceCollection
-     * @param array $data
-     */
-    public function __construct(
-        \Magento\Framework\Model\Context $context,
-        \Magento\Framework\Registry $registry,
-        \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory,
-        \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory,
-        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
-        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
-        array $data = []
-    ) {
-        parent::__construct(
-            $context,
-            $registry,
-            $extensionFactory,
-            $customAttributeFactory,
-            $resource,
-            $resourceCollection,
-            $data
-        );
-    }
-
-    /**
      * @inheritdoc
      *
      * @SuppressWarnings(PHPMD.CamelCaseMethodName)
